33fdaaa15ce2c04024ccb087b3349ee27ac1995e
John <john_melegrito@yahoo.com>
2020-03-19 02:58:10 -0500

    setting up parent-child comps

--- src/app/check-tournament-parent/check-tournament-parent.component.ts
+++ src/app/check-tournament-parent/check-tournament-parent.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckTournamentParentComponent implements OnInit {
 
+  tournament = {
+    name: "dummy tournament"
+  }
   constructor() { }
 
   ngOnInit() {
