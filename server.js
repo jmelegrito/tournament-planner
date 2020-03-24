f4fa122d72e89654a5405ca5b8975c29ef1756d4
John <john_melegrito@yahoo.com>
2020-03-24 03:31:28 -0500

    troubleshooting

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(express.static(__dirname + 'dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname, 'dist/tournament-planner/index.html'))
+res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
