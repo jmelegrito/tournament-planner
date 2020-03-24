baf3dc1b8ac8475d30fbf87a45b3d5f441baa39a
John <john_melegrito@yahoo.com>
2020-03-24 02:52:27 -0500

    adding something

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'dist'));
 
 app.get('*', function(req,res) {
     
