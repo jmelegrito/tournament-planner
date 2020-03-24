7c62befb6fddf8e46e620e23c725b664ade9d949
John <john_melegrito@yahoo.com>
2020-03-24 03:55:15 -0500

    fixing routes

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname, '/dist/index.html'))
 });
