7d0eca6c8b7b77400bee148d921c71beccc1f7c6
John <john_melegrito@yahoo.com>
2020-03-24 03:39:08 -0500

    fixing urls

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname, 'dist/index.html'))
+res.sendFile(path.join(__dirname, '/dist/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
